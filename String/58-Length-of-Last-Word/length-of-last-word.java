28ae6cdc6bc57e83840901bd0847a7c08b2a09aa
Abhay Pratap <theabhay101@gmail.com>
2026-05-22 07:51:03 +0000

    Time: 0 ms (100.00%) | Memory: 43.00 MB (67.72%) - LeetSync

--- String/58-Length-of-Last-Word/length-of-last-word.java
+++ String/58-Length-of-Last-Word/length-of-last-word.java
@@ -1,7 +1,22 @@
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         s = s.trim();
+//         int lastSpace = s.lastIndexOf(' ');
+//         return s.length() - lastSpace - 1;
+//     }
+// }
+
 class Solution {
     public int lengthOfLastWord(String s) {
-        s = s.trim();
-        int lastSpace = s.lastIndexOf(' ');
-        return s.length() - lastSpace - 1;
+        int length=0;
+        int i=s.length()-1;
+        while(i>=0 && s.charAt(i)==' '){
+            i--;
+        }
+        while(i>=0 && s.charAt(i)!=' '){
+            length++;
+            i--;
+        }
+        return length;
     }
 }
