8b4652457c3dcfb2cb2b85a8189748cce5969ed6
Abhay Pratap <theabhay101@gmail.com>
2026-06-05 08:36:53 +0000

    Time: 2 ms (100.00%) | Memory: 46.04 MB (68.12%) - LeetSync

---
+++ Dynamic Programming/300-Longest-Increasing-Subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,15 @@
+// Time Complexity: O(n log n) Space Complexity: O(n)
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp=new int[nums.length];
+        int len=0;
+        for(int n:nums){
+            int i=Arrays.binarySearch(dp,0,len,n);
+            if(i<0) i=-(i+1);
+            dp[i]=n;
+            if(i==len) len++;
+        }
+        return len;
+    }
+}
