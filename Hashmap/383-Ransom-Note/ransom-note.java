0b6379bf61f6d37d7d6efb9aa5c7c2c87c56203b
Abhay Pratap <theabhay101@gmail.com>
2026-07-03 08:14:22 +0000

    Time: 2 ms (54.90%) | Memory: 46.30 MB (79.08%) - LeetSync

---
+++ Hashmap/383-Ransom-Note/ransom-note.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+        // Count magazine letters
+        for (char c : magazine.toCharArray()) {
+            count[c - 'a']++;
+        }
+        // Use letters for ransom note
+        for (char c : ransomNote.toCharArray()) {
+            count[c - 'a']--;
+            if (count[c - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
