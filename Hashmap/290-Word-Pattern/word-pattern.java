3d540c75b05fa24578ac377a085bd86e24f55b80
Abhay Pratap <theabhay101@gmail.com>
2026-07-04 10:08:00 +0000

    Time: 1 ms (83.42%) | Memory: 42.30 MB (98.53%) - LeetSync

---
+++ Hashmap/290-Word-Pattern/word-pattern.java
@@ -0,0 +1,18 @@
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (pattern.length() != words.length)
+            return false;
+        Map<Object, Integer> map = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            Integer p = map.put(pattern.charAt(i), i);
+            Integer w = map.put(words[i], i);
+            if (!Objects.equals(p, w))
+                return false;
+        }
+        return true;
+    }
+}
