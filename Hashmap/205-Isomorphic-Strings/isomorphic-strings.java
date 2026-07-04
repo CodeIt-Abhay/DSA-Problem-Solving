c605c1993f6166c1f6eebe6a26cf8f30c319ca06
Abhay Pratap <theabhay101@gmail.com>
2026-07-04 09:48:04 +0000

    Time: 6 ms (89.05%) | Memory: 43.70 MB (73.88%) - LeetSync

--- Hashmap/205-Isomorphic-Strings/isomorphic-strings.java
+++ Hashmap/205-Isomorphic-Strings/isomorphic-strings.java
@@ -1,3 +1,4 @@
+//Arrays based solution
 //Time Complexity: O(n)
 //Space Complexity: O(1)
 
@@ -15,6 +16,36 @@ class Solution {
             }
             indexS[s.charAt(i)] = i + 1;
             indexT[t.charAt(i)] = i + 1;
+        }
+        return true;
+    }
+}
+
+//Hashmap based solution
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
+        Map<Character, Character> mapST = new HashMap<>();
+        Map<Character, Character> mapTS = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if (mapST.containsKey(c1)) {
+                if (mapST.get(c1) != c2)
+                    return false;
+            } else {
+                mapST.put(c1, c2);
+            }
+            if (mapTS.containsKey(c2)) {
+                if (mapTS.get(c2) != c1)
+                    return false;
+            } else {
+                mapTS.put(c2, c1);
+            }
         }
         return true;
     }
